80cdb9f205a2a7c0e2178dd74643829150d3ef1b
3tilley <1138504+3tilley@users.noreply.github.com>
2023-01-05 22:47:18 +0000

    Day 14 completion

--- r2022/src/day14.rs
+++ r2022/src/day14.rs
@@ -43,50 +43,24 @@ impl ToString for Cave {
 }
 
 impl Cave {
-    pub fn drop_grain(&mut self) -> (Option<(usize, usize)>, usize) {
-        let mut iterations = 0;
-        while let Some((xy, reason)) = self.next_spot() {
-            println!("Iterations: {}. (x, y): {:?} \n {}", iterations, xy, self.to_string());
-            iterations += 1;
-            self.grid[xy.0][xy.1] = reason;
-            self.update_current(xy);
-            if let Square::FallSand = reason {
-                return (Some(xy), iterations)
-            };
-        }
-        (None, iterations)
-    }
-
-    fn update_current(&mut self, new_pos: (usize, usize)) {
-        let old = self.current_grain;
-        self.grid[old.0][old.1] = Square::Empty;
-        self.current_grain = new_pos;
-    }
-
-    fn next_spot(&self) -> Option<((usize, usize), Square)> {
-        [(0,1), (-1,1), (1,1), (0,0)].into_iter().filter_map(|(x_diff, y_diff)| {
-            let x = add(self.current_grain.0, x_diff).unwrap();
-            let y = add(self.current_grain.1, y_diff).unwrap();
-            match self.grid[x][y] {
-                Square::Empty => Some(Some(((x,y), Square::FallSand))),
-                Square::HardSand | Square::Rock => None,
-                Square::FallSand => Some(Some(((x,y), Square::FallSand))),
-            }
-        }).into_iter().nth(0).unwrap_or(None)
-    }
-}
-
-impl FromStr for Cave {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    pub fn from_string(s: &str, add_floor: bool) -> Result<Cave, ()> {
         let rocks: Vec<Vec<(usize, usize)>> = s.lines().map(|s_| s_.split(" -> ").map(|xy| {
             let (x, y) = xy.split_once(",").unwrap();
             (x.parse().unwrap(), y.parse().unwrap())
         }).collect()).collect();
-        let x_max = rocks.iter().map(|rock_line| rock_line.iter().map(|&r| r.0).max().unwrap()).max().unwrap() + 2;
-        let y_max = rocks.iter().map(|rock_line| rock_line.iter().map(|&r| r.1).max().unwrap()).max().unwrap() + 2;
-        let mut grid = vec![vec![Square::Empty; y_max]; x_max];
+        let mut x_max = rocks.iter().map(|rock_line| rock_line.iter().map(|&r| r.0).max().unwrap()).max().unwrap() + 2;
+        let mut y_max = rocks.iter().map(|rock_line| rock_line.iter().map(|&r| r.1).max().unwrap()).max().unwrap() + 2;
+        let mut grid = if add_floor {
+            y_max += 1;
+            x_max += y_max;
+            let mut g = vec![vec![Square::Empty; y_max]; x_max];
+            for x in 0..(x_max) {
+                g[x][y_max-1] = Square::Rock;
+            }
+            g
+        } else {
+            vec![vec![Square::Empty; y_max]; x_max]
+        };
         for rock in rocks {
             let start_end = rock[0..rock.len()-1].iter().zip(rock[1..].iter());
             for (start, end) in start_end {
@@ -101,13 +75,70 @@ impl FromStr for Cave {
             }
         }
         let current_grain = (500, 0);
-        grid[current_grain.0][current_grain.1];
+        grid[current_grain.0][current_grain.1] = Square::FallSand;
         Ok(Cave {grid, x_max, y_max, current_grain})
+    }
+
+pub fn drop_grain(&mut self) -> (Option<(usize, usize)>, usize) {
+    let mut iterations = 0;
+    while let Some((xy, reason)) = self.next_spot() {
+        // println!("Iterations: {}. (x, y): {:?} \n {}", iterations, xy, self.to_string());
+            iterations += 1;
+            self.update_current(xy, reason);
+            if let Square::HardSand = reason {
+                return (Some(xy), iterations)
+            };
+        }
+        (None, iterations)
+    }
+
+    fn update_current(&mut self, new_pos: (usize, usize), square: Square) {
+        let old = self.current_grain;
+        self.grid[old.0][old.1] = Square::Empty;
+        self.grid[new_pos.0][new_pos.1] = square;
+        self.current_grain = new_pos;
+    }
+
+    fn reset(&mut self) -> bool {
+        self.current_grain = (500,0);
+        if self.grid[self.current_grain.0][self.current_grain.1] == Square::HardSand {
+            false
+        } else {
+            self.grid[self.current_grain.0][self.current_grain.1] = Square::FallSand;
+            true
+        }
+    }
+
+    fn next_spot(&self) -> Option<((usize, usize), Square)> {
+        if self.current_grain.1 > self.y_max - 2 {
+            return None;
+        }
+        [(0,1), (-1,1), (1,1)].into_iter().filter_map(|(x_diff, y_diff)| {
+            let x = add(self.current_grain.0, x_diff).unwrap();
+            let y = add(self.current_grain.1, y_diff).unwrap();
+            match self.grid[x][y] {
+                Square::Empty => Some(Some(((x,y), Square::FallSand))),
+                Square::HardSand | Square::Rock => None,
+                Square::FallSand => Some(Some(((x,y), Square::FallSand))),
+            }
+        }).into_iter().nth(0).unwrap_or(Some(((self.current_grain.0, self.current_grain.1), Square::HardSand)))
+    }
+}
+
+impl FromStr for Cave {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Cave::from_string(s, false)
     }
 }
 
 pub fn prepare(input: String) -> Input1 {
     input.parse().unwrap()
+}
+
+pub fn prepare_2(input: String) -> Input2 {
+    Cave::from_string(&*input, true).unwrap()
 }
 
 pub fn part_1(mut input: Input1) -> Output1 {
@@ -116,7 +147,7 @@ pub fn part_1(mut input: Input1) -> Output1 {
     let mut iterations = 0;
     println!("{}", input.to_string());
     while let None = finish {
-        println!("Particles: {}. Iterations: {}", particles, iterations);
+        // println!("Particles: {}. Iterations: {}", particles, iterations);
         particles += 1;
         match input.drop_grain() {
             (None, n) => {
@@ -124,26 +155,46 @@ pub fn part_1(mut input: Input1) -> Output1 {
                 finish = Some(particles)
             },
             (Some((x, y)), n) => {
-                iterations += x;
-                input.update_current((500, 0));
+                iterations += n;
+                if !input.reset() {finish = Some(particles)};
             }
         }
     }
+    println!("{}", input.to_string());
     finish.unwrap() - 1
 }
 
-pub fn part_2(input: Input2) -> Output2 {
-    todo!("Implement part 2")
+pub fn part_2(mut input: Input2) -> Output2 {
+    let mut finish = None;
+    let mut particles = 0;
+    let mut iterations = 0;
+    println!("{}", input.to_string());
+    while let None = finish {
+        // println!("Particles: {}. Iterations: {}", particles, iterations);
+        particles += 1;
+        match input.drop_grain() {
+            (None, n) => {
+                iterations += n;
+                finish = Some(particles)
+            },
+            (Some((x, y)), n) => {
+                iterations += n;
+                if !input.reset() {finish = Some(particles)};
+            }
+        }
+    }
+    println!("{}", input.to_string());
+    finish.unwrap()
 }
 
 pub fn make_sol() -> StructSolution<Input1, Output1, Input2, Output2> {
     let struct_solution = StructSolution {
         prepare_part_1: prepare,
         calc_part_1: part_1,
-        prepare_part_2: prepare,
+        prepare_part_2: prepare_2,
         calc_part_2: part_2,
-        example_part_1: Example::Value(14),
-        example_part_2: Example::Value(0),
+        example_part_1: Example::Value(24),
+        example_part_2: Example::Value(93),
         day_data: DayData::new(14, false),
     };
     struct_solution
